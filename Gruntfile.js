dbaf95dc0b0e84da5914259d1ebd062a7b4e3d39
sixertoy <sixertoy.github@gmail.com>
2015-07-07 12:13:49 +0200

    bump version

---
+++ Gruntfile.js
@@ -0,0 +1,27 @@
+module.exports = function (grunt) {
+    'use strict';
+    // Project configuration
+    grunt.initConfig({
+        bump: {
+            options: {
+                files: ['package.json'],
+                updateConfigs: [],
+                commit: false,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: ['package.json'],
+                createTag: false,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: false,
+                pushTo: 'upstream',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false,
+                prereleaseName: false,
+                regExp: false
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks
+    grunt.loadNpmTasks('grunt-bump');
+};
--- README.md
+++ README.md
@@ -18,6 +18,46 @@ yo file <path/to/file_name>
 - PHP
 - HTML
 
+### Templates
+
+#### HTML
+
+```html
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <title></title>
+        <meta charset="utf-8" />
+        <meta http-equiv="x-ua-compatible" content="ie=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1" />
+    </head>
+    <body>
+
+    </body>
+</html>
+```
+
+#### JavaScript (CommonJS)
+
+```javascript
+/*jslint indent: 4, nomen: true, plusplus: true */
+/*globals require, module */
+(function(){
+
+    'use strict';
+
+    module.exports = {};
+
+}());
+```
+
+#### PHP
+
+```php
+<?php
+
+```
+
 [license-img]: http://img.shields.io/badge/license-MIT-blue.svg?style=flat-square
 [license-url]: LICENSE-MIT
 
